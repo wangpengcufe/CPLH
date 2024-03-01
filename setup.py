17ce736efcc309141bac7c5459a5b6504d3bab5f
wangpengcufe <wangpengcufe@163.com>
2024-03-01 19:07:00 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+* Author      : wangpeng
+* Email       : wangpeng@cnpc.com.cn
+* Create time : 2024/3/1 17:13
+* File name   : setup.py
+* Reference   :
+* Description :
+"""
+
+from setuptools import setup, find_packages
+
+setup(name='cplhtools',
+      version='0.1.1',
+      description='CPLH Algorithm Develop Tools',
+      author='wangpeng',
+      author_email='wangpeng@cnpc.com.cn',
+      packages=find_packages()
+     )
