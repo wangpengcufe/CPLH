e8b56dcadedfbb9cbe5869f64112caac611803f2
wangpengcufe <wangpengcufe@163.com>
2024-03-01 19:05:53 +0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+"""
+* Author      : wangpeng
+* Email       : wangpeng@cnpc.com.cn
+* Create time : 2024/3/1 19:05
+* File name   : __init__
+* Reference   :
+* Description :
+"""
+from .db_util import DB
